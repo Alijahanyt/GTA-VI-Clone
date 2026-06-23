b2c67769ac1e2187f7f28db97c486739e55a055e
Ali jahan <ali.internet7@gmail.com>
2026-06-24 03:21:17 +0330

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,19 +1,19 @@
 import gsap from "gsap"
 import { useGSAP } from "@gsap/react"
-import {useMaskSettings} from '../../constants'
+import { useMaskSettings } from '../../constants'
 import ComingSoon from "./ComingSoon"
 
 const Hero = () => {
 
-  const {initialMaskPos,initialMaskSize,maskPos,maskSize} = useMaskSettings()
-  useGSAP(()=>{
-    gsap.set('.mask-wrapper',{
+  const { initialMaskPos, initialMaskSize, maskPos, maskSize } = useMaskSettings()
+  useGSAP(() => {
+    gsap.set('.mask-wrapper', {
       maskPosition: initialMaskPos,
       maskSize: initialMaskSize,
     })
     gsap.set('.mask-logo', {
       marginTop: '-100vh',
-      opacity:0,
+      opacity: 0,
 
     })
     gsap.set('.entrance-message', {
@@ -22,7 +22,7 @@ const Hero = () => {
     })
 
     const tl = gsap.timeline({
-      scrollTrigger:{
+      scrollTrigger: {
         trigger: '.hero-section',
         start: 'top top',
         scrub: 2.5,
@@ -32,33 +32,35 @@ const Hero = () => {
     })
 
     tl
-    .to('.fade-out', {
-      opacity:0,
-      ease: 'power1.inOut'
-    })
-    .to('.scale-out', {
-      scale: 1,
-      ease: 'Power1.inOut',
-    })
-    .to('.mask-wrapper', {
-      maskSize,
-      ease: 'power1.inOut'
-    }, '<')
-    .to('.mask-wrapper', {
-      opacity: 0,
+      .to('.fade-out', {
+        opacity: 0,
+        ease: 'power1.inOut'
+      })
+      .to('.scale-out', {
+        scale: 1,
+        ease: 'power1.inOut',
+      })
+      .to('.mask-wrapper', {
+        maskSize,
+        ease: 'power1.inOut'
+      }, '<')
+      .to('.mask-wrapper', {
+        opacity: 0,
 
-    })
-    .to('.overlay-logo', {
-      opacity: 1,
-      onComplete: ()=>{
-        gsap.to('.overlay-logo', {opacity:0})
-      }
-    },"<")
-    .to("entrance-message", {
-      duration: 1,
-      ease: 'power1.inOut',
-      maskImage: 'radial-gradient(circle at 50% 0vh, black 50%, transparent 100%)'
-    }, '<')
+      })
+      .to('.overlay-logo', {
+        opacity: 1,
+        onComplete: () => {
+          gsap.to('.overlay-logo', {
+            opacity: 0,
+          })
+        }
+      }, "<")
+      .to('.entrance-message', {
+        duration: 1,
+        ease: 'power1.inOut',
+        maskImage: 'radial-gradient(circle at 50% 0vh, black 50%, transparent 100%)'
+      }, '<')
 
   })
 
@@ -71,16 +73,15 @@ const Hero = () => {
         <div className="play-img fade-out">
           <img src="/images/play.png" alt="play" className="w-7 ml-1" />
         </div>
-
       </div>
 
       <div>
         <img src="/images/big-hero-text.svg" alt="logo" className="size-full object-cover mask-logo" />
-        <div className="fake-logo-wrapper">
-          <img src="/images/big-hero-text.svg" alt="logo" className="overlay-logo" />
-        </div>
       </div>
-      <ComingSoon/>
+      <div className="fake-logo-wrapper">
+        <img src="/images/big-hero-text.svg" alt="logo" className="overlay-logo" />
+      </div>
+      <ComingSoon />
     </section>
   )
 }
